22dca4ecf2168649f3239b3cc86c885ee8d09b6d
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-08-28 18:08:43 +0000

    Create login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
-	return <h1>Hello!!! This is the feature-branch</h1>;
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/signup"
+					element={<Signup />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/components/Header_Login_Signup.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header_Login_Signup = () => {
+	const [link, setLink] = useState("");
+
+	useEffect(() => {
+		if (window.location.pathname === "/login") {
+			setLink("login");
+		} else if (window.location.pathname === "/signup") {
+			setLink("signup");
+		}
+	}, []);
+	return (
+		<header>
+			<h1 className="logo">Brain Deck</h1>
+
+			<ul>
+				<li>About</li>
+				<Link to={link === "login" ? "/signup" : "/login"}>
+					<li>{link === "login" ? "Signup" : "Login"}</li>
+				</Link>
+			</ul>
+		</header>
+	);
+};
+
+export default Header_Login_Signup;
